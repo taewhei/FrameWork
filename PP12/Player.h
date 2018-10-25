b6d342b9e55f4a9dfca8ee865b3b0d77e94f83c8
태휘 전 <20171270@vision.hoseo.edu>
2018-10-25 18:12:59 +0900

    PP11,PP12

---
+++ PP12/Player.h
@@ -0,0 +1,12 @@
+#pragma once
+#include"SDLGameObject.h"
+
+class Player : public SDLGameObject
+{
+public:
+	Player(const LoaderParams* pParams);
+	virtual void draw();
+	virtual void update();
+	virtual void clean();
+};
+
