97a5efbbb348d3640a7c4336c3c1b73713df7926
태휘 전 <20171270@vision.hoseo.edu>
2018-11-20 12:37:03 +0900

    ...

--- PP14.MInputHandler/Enemy.cpp
+++ PP14.MInputHandler/Enemy.cpp
@@ -16,7 +16,7 @@ void Enemy::update()
 	//
 	//m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 	handleInput(); // add our function
-	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));//...
 	SDLGameObject::update();
 }
 
